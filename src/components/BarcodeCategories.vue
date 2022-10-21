d792b9a4b3cb8304266497a4d680cad214135485
clara79dev <clara79dev@gmail.com>
2022-10-21 23:52:43 +0000

    first commit

---
+++ src/components/BarcodeCategories.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "BarcodeCategories",
+  props: {
+    categories: Array
+  },
+  emits: ["category-selected"]
+};
+</script>
+
+<template>
+  <h2>Barcode Categories</h2>
+  <ul v-if="categories.length">
+    <li v-for="category in categories" :key="category">
+      <a href="#" @click.prevent="$emit('category-selected', category)">
+        <h3>{{ category }}</h3>
+      </a>
+    </li>
+  </ul>
+  <p v-else>No barcodes available</p>
+</template>
+
+<style scoped></style>
