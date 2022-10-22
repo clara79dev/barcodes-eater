bcdd655144afc95e5e7e7ce0478a7ab3ea7cbf1c
clara79dev <clara79dev@gmail.com>
2022-10-22 14:03:12 +0000

    Update 2 files

---
+++ src/components/BarcodeList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "BarcodeList",
+  props: {
+    category: String,
+    barcodes: Array
+  },
+  emits: ["close-request"]
+};
+</script>
+
+<template>
+  <div>
+    <a href="#" @click.prevent="$emit('close-request')">
+      &lt;- Torna alle categorie
+    </a>
+    <h2>{{ category }} Barcodes</h2>
+    <ul v-if="barcodes.length">
+      <li v-for="barcode in barcodes" :key="barcode.code">
+        <p>{{ barcode.code }}</p>
+        <p>{{ barcode.reductionStatus }}</p>
+      </li>
+    </ul>
+    <p v-else>No barcodes available</p>
+  </div>
+</template>
+
+<style scoped></style>
